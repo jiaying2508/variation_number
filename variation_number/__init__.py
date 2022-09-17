655c74599c51de133409fea67eda5cd64d1386c5
Jiaying <jiaying_lai@brown.edu>
2022-09-17 15:55:09 +0800

    Update __init__.py

--- variation_number/__init__.py
+++ variation_number/__init__.py
@@ -5,4 +5,4 @@ Python package to calculate variation number
 variation_number is a package for calculating the variation number for gene sequences.
 """
 
-__version__ = "0.2.1"
+__version__ = "0.2.3"
